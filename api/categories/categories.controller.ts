1efb535f9fdb4de17eb46d8f642a0a272085ba1d
Bryan <david_bryan34@hotmail.com>
2023-01-30 17:39:05 +0000

    Create endpoint to find commands Ok

--- api/categories/categories.controller.ts
+++ api/categories/categories.controller.ts
@@ -17,15 +17,17 @@ export const findFilters = async(req: any, res: any) => {
 }
 
 /**
- * Find commands by command or info. Info can be in 'en' or 'es' 
+ * Find commands by command, lang or meaning. Lang can be in 'en' or 'es'.
+ * Having in consideration upper and lower case 
  * Path:
  *      lang: [compulsory] 'en' or 'es'
  * Queryparams:
- *      category: [compulsory] with default 'all'
+ *      category: [compulsory] with default 'all'. It must be id of MongoDB
  *      commands
+ *      meaning
  */
 export const searchCommands = async(req: any, res: any) => {
-    const {category, command} = req.query;
+    const {category, command, meaning} = req.query;
     const {lang} = req.params;
 
     // Validations
@@ -34,23 +36,50 @@ export const searchCommands = async(req: any, res: any) => {
             msg: "Not found"
         });
     }
+    if(!(lang === "en" || lang === "es")){
+        return res.status(httpCodes.not_found).json({
+            msg: "Not found"
+        });
+    }
     
-    // If category = all
     let result: any[] = [];
+    // 1º Case
+    // If category = all
     if(category === "all"){
         let found: any = await CategoriesModel.find();
         const commands = JSON.parse(JSON.stringify(found));
         // All in one buffer, is easer to work
         commands.map( (item: any) => {
-            item.commands.map((element: any) => {
-                if(element.command.includes(command)){
-                    result.push(element);        
+            item.commands?.map((element: any) => {
+                // Find by query command or meaning
+                if(element.command.toLowerCase().includes( command.toLowerCase() ) ||
+                element[lang].toLowerCase().includes( meaning.toLowerCase() )){
+                    result.push({ 
+                        command: element.command,
+                        [lang]: element[lang]
+                    });        
                 }
             });
         });
 
+    // 2º Case
+    }else{
+        let found: any = await CategoriesModel.findById(category);
+        const commands = JSON.parse(JSON.stringify(found));
 
-    }   
+        // All in one buffer, is easer to work
+        commands.commands?.map((element: any) => {
+            // Find by query command or meaning
+            console.log("element: ", element)
+            if(element.command.toLowerCase().includes( command.toLowerCase() ) ||
+            element[lang].toLowerCase().includes( meaning.toLowerCase() )){
+                result.push({ 
+                    command: element.command,
+                    [lang]: element[lang]
+                });        
+            }
+        });
+    }  
 
     return res.json(result);
 }
