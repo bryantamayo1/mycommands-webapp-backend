29e09999d1394b1fe06f77ecf96ee2b1f1076d17
Bryan <david_bryan34@hotmail.com>
2023-03-03 15:11:19 +0000

    Test in prod v16 ...

--- server.ts
+++ server.ts
@@ -20,13 +20,13 @@ process.on("uncaughtException", (err, origin) => {
 if(process.env.NODE_ENV === 'development'){
     dotenv.config({ path: "./.env.development.local" });
     console.log("[SO] "+process.env.SO);
-    console.log("[mode] development");
+    console.log("[mode] "+process.env.NODE_ENV);
 }else{
     if(process.env.SO !== 'linux'){
         dotenv.config({ path: "./.env.production.local" });
     }
     console.log("[SO] "+process.env.SO);
-    console.log("[mode] production");
+    console.log("[mode] "+process.env.NODE_ENV);
 }
 
 const server = new Server();
